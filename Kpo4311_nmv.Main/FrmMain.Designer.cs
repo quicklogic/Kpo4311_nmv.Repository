2a306ac0bcdfffa7c399779d25a29d7b3546c7cc
Niyaz Khusnutdinov <zarrat97@gmail.com>
2017-09-26 23:01:40 +0300

    Добавьте файлы проекта.

---
+++ Kpo4311_nmv.Main/FrmMain.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Kpo4311.nmv.Main
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MMenu = new System.Windows.Forms.MenuStrip();
+            this.mmFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.SStatus = new System.Windows.Forms.StatusStrip();
+            this.MMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MMenu
+            // 
+            this.MMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mmFile});
+            this.MMenu.Location = new System.Drawing.Point(0, 0);
+            this.MMenu.Name = "MMenu";
+            this.MMenu.Size = new System.Drawing.Size(284, 24);
+            this.MMenu.TabIndex = 0;
+            this.MMenu.Text = "menuStrip1";
+            // 
+            // mmFile
+            // 
+            this.mmFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnExit,
+            this.mnOpen});
+            this.mmFile.Name = "mmFile";
+            this.mmFile.Size = new System.Drawing.Size(57, 20);
+            this.mmFile.Text = "1&.Файл";
+            // 
+            // mnExit
+            // 
+            this.mnExit.Name = "mnExit";
+            this.mnExit.Size = new System.Drawing.Size(152, 22);
+            this.mnExit.Text = "Выход";
+            this.mnExit.Click += new System.EventHandler(this.mnExit_Click);
+            // 
+            // mnOpen
+            // 
+            this.mnOpen.Name = "mnOpen";
+            this.mnOpen.Size = new System.Drawing.Size(152, 22);
+            this.mnOpen.Text = "Открыть";
+            this.mnOpen.Click += new System.EventHandler(this.mnOpen_Click);
+            // 
+            // SStatus
+            // 
+            this.SStatus.Location = new System.Drawing.Point(0, 239);
+            this.SStatus.Name = "SStatus";
+            this.SStatus.Size = new System.Drawing.Size(284, 22);
+            this.SStatus.TabIndex = 1;
+            this.SStatus.Text = "statusStrip1";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.SStatus);
+            this.Controls.Add(this.MMenu);
+            this.MainMenuStrip = this.MMenu;
+            this.Name = "FrmMain";
+            this.Text = "КПО:4311:Хуснутдинов";
+            this.MMenu.ResumeLayout(false);
+            this.MMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MMenu;
+        private System.Windows.Forms.ToolStripMenuItem mmFile;
+        private System.Windows.Forms.ToolStripMenuItem mnExit;
+        private System.Windows.Forms.ToolStripMenuItem mnOpen;
+        private System.Windows.Forms.StatusStrip SStatus;
+    }
+}
+
---
+++ Kpo4311_nmv.Main/FrmMain.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kpo4311.nmv.Main
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void mnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void mnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                throw new NotImplementedException();
+
+               //throw new Exception("Неправильные входные параметры");
+            }
+            catch(NotImplementedException ex)
+            {
+                MessageBox.Show("Ошибка №1:" + ex.Message);
+                Lib.LogUtility.ErrorLog(ex.Message);                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ошибка №2:" + ex.Message);
+            }
+        }
+    }
+}
